359a9e160f9df02192bb469bc94e871c1e9a3ed3
Boudewijn Schoon <boudewijn@zicht.nl>
2015-11-03 12:59:10 +0000

    implements Countable interface for MapIterator
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtIter/trunk@72259 0f35748b-8228-47f0-bbea-889afbbd9a42

--- src/Zicht/Itertools/lib/MapIterator.php
+++ src/Zicht/Itertools/lib/MapIterator.php
@@ -2,18 +2,22 @@
 
 namespace Zicht\Itertools\lib;
 
+use Closure;
+use Countable;
 use InvalidArgumentException;
+use Iterator;
+use MultipleIterator;
 
-class MapIterator extends \MultipleIterator
+class MapIterator extends MultipleIterator implements Countable
 {
     private $func;
     private $key;
 
-    public function __construct(\Closure $func /* \Iterator $iterable1, \Iterator $iterable2, ... */)
+    public function __construct(Closure $func /* \Iterator $iterable1, \Iterator $iterable2, ... */)
     {
-        parent::__construct(\MultipleIterator::MIT_NEED_ALL|\MultipleIterator::MIT_KEYS_NUMERIC);
+        parent::__construct(MultipleIterator::MIT_NEED_ALL| MultipleIterator::MIT_KEYS_NUMERIC);
         foreach (array_slice(func_get_args(), 1) as $iterable) {
-            if (!$iterable instanceof \Iterator) {
+            if (!$iterable instanceof Iterator) {
                 throw new InvalidArgumentException(sprintf('Argument %d must be an iterator'));
             }
             $this->attachIterator($iterable);
@@ -47,5 +51,19 @@ class MapIterator extends \MultipleIterator
     public function toArray()
     {
         return iterator_to_array($this);
+    }
+
+    /**
+     * Count elements of an object
+     * @link http://php.net/manual/en/countable.count.php
+     * @return int The custom count as an integer.
+     * </p>
+     * <p>
+     * The return value is cast to an integer.
+     * @since 5.1.0
+     */
+    public function count()
+    {
+        return iterator_count($this);
     }
 }
