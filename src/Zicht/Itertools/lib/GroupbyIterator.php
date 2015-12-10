835e55993c50f42e54867d22a76b9c866be6d755
Rik van der Kemp <rik@zicht.nl>
2015-12-10 10:50:51 +0000

    implemented array access
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtIter/trunk@75219 0f35748b-8228-47f0-bbea-889afbbd9a42

--- src/Zicht/Itertools/lib/GroupbyIterator.php
+++ src/Zicht/Itertools/lib/GroupbyIterator.php
@@ -83,7 +83,7 @@ class GroupedIterator extends IteratorIterator implements Countable, ArrayAccess
     }
 }
 
-class GroupbyIterator extends IteratorIterator implements Countable
+class GroupbyIterator extends IteratorIterator implements Countable, ArrayAccess
 {
     public function __construct(Closure $func, Iterator $iterable)
     {
@@ -114,6 +114,51 @@ class GroupbyIterator extends IteratorIterator implements Countable
     public function count()
     {
         return iterator_count($this->getInnerIterator());
+    }
+
+    /**
+     * @param mixed $offset
+     * @return bool
+     */
+    public function offsetExists($offset)
+    {
+        foreach ($this as $key => $_) {
+            if ($key === $offset) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @param mixed $offset
+     * @return mixed|null
+     */
+    public function offsetGet($offset, $default = null)
+    {
+        foreach ($this as $key => $value) {
+            if ($key === $offset) {
+                return $value;
+            }
+        }
+        return $default;
+    }
+
+    /**
+     * @param mixed $offset
+     * @param mixed $value
+     */
+    public function offsetSet($offset, $value)
+    {
+        throw new \RuntimeException('It is not possible to set iterator values');
+    }
+
+    /**
+     * @param mixed $offset
+     */
+    public function offsetUnset($offset)
+    {
+        throw new \RuntimeException('It is not possible to unset iterator values');
     }
 
     public function toArray()
