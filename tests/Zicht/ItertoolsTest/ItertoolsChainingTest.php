3177e029623e955ce97f7b423f65f4cd8962ce58
Boudewijn Schoon <boudewijn@zicht.nl>
2016-06-23 08:45:47 +0000

    add chain test in one go
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtIter/branches/itertool-chaining-trait@86576 0f35748b-8228-47f0-bbea-889afbbd9a42

--- tests/Zicht/ItertoolsTest/ItertoolsChainingTest.php
+++ tests/Zicht/ItertoolsTest/ItertoolsChainingTest.php
@@ -9,16 +9,28 @@ class ItertoolsChainingTest extends PHPUnit_Framework_TestCase
 {
     public function testChain()
     {
-        $expected = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
+        $multiply = function ($value) { return $value * 2; };
+
+        $originalData = $expected = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
         $iterator = iter\chain($expected);
         $this->assertInstanceOf('\Zicht\Itertools\lib\ChainIterator', $iterator);
         $this->assertEquals($expected, $iterator->toArray());
 
         $expected = array(2, 4, 6, 8, 10, 12, 14, 16, 18, 0);
-        $iterator = $iterator->map(function ($value) { return $value * 2; });
+        $iterator = $iterator->map($multiply);
         $this->assertInstanceOf('\Zicht\Itertools\lib\MapIterator', $iterator);
         $this->assertEquals($expected, $iterator->toArray());
 
         # todo: add more!
+
+        // entire chain in one go
+        $iterator = iter\chain($originalData)->map($multiply);
+        $this->assertInstanceOf('\Zicht\Itertools\lib\MapIterator', $iterator);
+        $this->assertEquals($expected, $iterator->toArray());
+
+        // entire chain written without the chaining feature
+        $iterator = iter\map($multiply, iter\chain($originalData));
+        $this->assertInstanceOf('\Zicht\Itertools\lib\MapIterator', $iterator);
+        $this->assertEquals($expected, $iterator->toArray());
     }
 }
