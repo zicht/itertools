0b8aba199ca3f1fb0ac94609d2486870bab06e68
Boudewijn Schoon <boudewijn@zicht.nl>
2016-06-30 17:32:00 +0200

    adds tests for (deprecated) uniqueBy

--- tests/Zicht/ItertoolsTest/UniqueByTest.php
+++ tests/Zicht/ItertoolsTest/UniqueByTest.php
@@ -5,14 +5,14 @@ namespace Zicht\ItertoolsTest;
 use InvalidArgumentException;
 use PHPUnit_Framework_TestCase;
 
-class UniqueTest extends PHPUnit_Framework_TestCase
+class UniqueByTest extends PHPUnit_Framework_TestCase
 {
     /**
      * @dataProvider goodSequenceProvider
      */
     public function testGoodSequence(array $arguments, array $expectedKeys, array $expectedValues)
     {
-        $iterator = call_user_func_array('\Zicht\Itertools\unique', $arguments);
+        $iterator = call_user_func_array('\Zicht\Itertools\uniqueBy', $arguments);
         $this->assertInstanceOf('\Zicht\Itertools\lib\UniqueIterator', $iterator);
         $this->assertEquals(sizeof($expectedKeys), sizeof($expectedValues));
         $this->assertEquals(sizeof($iterator), sizeof($expectedKeys));
@@ -33,33 +33,6 @@ class UniqueTest extends PHPUnit_Framework_TestCase
     public function goodSequenceProvider()
     {
         return array(
-            // call WITHOUT $keyStrategy
-            array(
-                array(null),
-                array(),
-                array(),
-            ),
-            array(
-                array(array()),
-                array(),
-                array(),
-            ),
-            array(
-                array(array(1, 2, 3)),
-                array(0, 1, 2),
-                array(1, 2, 3)
-            ),
-            array(
-                array(array(1, 1, 2, 2, 3, 3)),
-                array(0, 2, 4),
-                array(1, 2, 3)
-            ),
-            array(
-                array(array(1, 2, 3, 3, 2, 1)),
-                array(0, 1, 2),
-                array(1, 2, 3)
-            ),
-
             // call WITH $keyStrategy
             array(
                 array(null, array(1, 2, 3)),
@@ -90,20 +63,19 @@ class UniqueTest extends PHPUnit_Framework_TestCase
      */
     public function testBadArgument(array $arguments)
     {
-        $iterator = call_user_func_array('\Zicht\Itertools\unique', $arguments);
+        $iterator = call_user_func_array('\Zicht\Itertools\uniqueBy', $arguments);
     }
 
     public function badArgumentProvider()
     {
         return array(
             // wrong types
-            array(array(0)),
-            array(array(1.0)),
-            array(array(true)),
-
-            // wrong argument count
-            array(array()),
-            array(array(function ($value) { return $value; }, array(1, 2, 3), 'one argument to many')),
+            array(array(null, 0)),
+            array(array(null, 1.0)),
+            array(array(null, true)),
+            array(array(0, array())),
+            array(array(1.0, array())),
+            array(array(true, array())),
         );
     }
 }
