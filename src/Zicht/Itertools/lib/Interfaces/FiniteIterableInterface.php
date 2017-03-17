2bd78ccc03f181ef1ccd643c7738db034690d06f
Boudewijn Schoon <boudewijn@zicht.nl>
2017-03-17 17:26:23 +0100

    Add Finite and Infinite interfaces and traits
    Every Iterator will fall into either the finite or the infinite
    category.  Each has its own interface definition and traits that are
    available by default.

---
+++ src/Zicht/Itertools/lib/Interfaces/FiniteIterableInterface.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @author Boudewijn Schoon <boudewijn@zicht.nl>
+ * @copyright Zicht Online <http://zicht.nl>
+ */
+
+namespace Zicht\Itertools\lib\Interfaces;
+
+/**
+ * Interface FiniteIterableInterface
+ *
+ * This interface declares everything that a finite iterator should be able to do.
+ * Note that it is a superset of the possibilities that an infinite iterator has, as
+ * some features require the entire iterator to be available, i.e. sorting.
+ *
+ * @package Zicht\Itertools\lib\Interfaces
+ */
+interface FiniteIterableInterface extends
+    \Countable,
+    \ArrayAccess,
+    InfiniteIterableInterface,
+    AllInterface,
+    AnyInterface,
+    ChainInterface,
+    CycleInterface,
+    FilterInterface,
+    GroupByInterface,
+    ItemsInterface,
+    KeysInterface,
+    LastInterface,
+    ReduceInterface,
+    ReversedInterface,
+    SortedInterface,
+    ToArrayInterface,
+    UniqueInterface,
+    ValuesInterface
+{
+}
---
+++ src/Zicht/Itertools/lib/Interfaces/InfiniteIterableInterface.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author Boudewijn Schoon <boudewijn@zicht.nl>
+ * @copyright Zicht Online <http://zicht.nl>
+ */
+
+namespace Zicht\Itertools\lib\Interfaces;
+
+/**
+ * Interface InfiniteIterableInterface
+ *
+ * This interface declares everything that an infinite iterator should be able to do.
+ * Note that it is a subset of the possibilities that a finite iterator has, as some
+ * features require the entire iterator to be available, i.e. sorting.
+ *
+ * @package Zicht\Itertools\lib\Interfaces
+ */
+interface InfiniteIterableInterface extends
+    \Iterator,
+    AccumulateInterface,
+    FirstInterface,
+    MapByInterface,
+    MapInterface,
+    SliceInterface,
+    ZipInterface
+{
+}
